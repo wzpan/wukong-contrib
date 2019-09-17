6f6b90e23d3694a8d05a7c48c6c1580e25880771
t0Lex <1198087737@qq.com>
2019-09-18 03:20:43 +0800

    feat(plugins) 上一commit是缩减技能关键字和多轮询问框架, 本次修改为继承MusicPlayer

--- WangYiYun.py
+++ WangYiYun.py
@@ -7,35 +7,29 @@ import subprocess
 import platform
 from random import shuffle
 from robot.sdk.AbstractPlugin import AbstractPlugin
-from robot.Player import SoxPlayer
+from robot.Player import MusicPlayer
 from robot import config, logging, constants, utils
 from sdk import NetEaseApi
 
 logger = logging.getLogger(__name__)
 
-class WangYiYunPlayer(SoxPlayer):
+class WangYiYunPlayer(MusicPlayer):
     """
     给网易云播放器插件使用的，
-    在 SOXPlayer 的基础上增加了列表的支持，支持暂停和恢复播放
-    并支持登陆和外接网易云API，返回相关信息
+    在 MusicPlayer 的基础上支持登陆和外接网易云API，返回相关信息。
     """
     SLUG = 'WangYiYunPlayer'
 
-    def __init__(self, account, md5pass, plugin, **kwargs):
-        super(WangYiYunPlayer, self).__init__(**kwargs)
+    def __init__(self, account, md5pass, playlist, plugin, **kwargs):
+        super(WangYiYunPlayer, self).__init__(playlist, plugin, **kwargs)
         self.account = account
         self.md5pass = md5pass
-        self.plugin = plugin
         self.userid = None
         self.nickname = None
         self.api = NetEaseApi.NetEase()
-        self.playlist = None
-        self.multi_playlist = None
-        self.random = False
-        self.idx = 0
+        self.playlist_info = None
+        self.multi_playlists = None
         self.list_idx = None
-        self.pausing = False
-        self.last_paused = None
 
         ## 首次登陆则需发起请求获取cookies，cookies过期则需发起请求
         if len(self.api.session.cookies) == 0:
@@ -45,6 +39,12 @@ class WangYiYunPlayer(SoxPlayer):
             self.userid = config.get('/' + self.plugin.SLUG + '/userid')
             self.nickname = config.get('/' + self.plugin.SLUG + '/nickname')
             self.daily_checkin()
+ 
+    def replay(self):
+        logger.debug('WangYiYunPlayer replay')
+        path = self.playlist[self.idx]
+        super().stop()
+        super(MusicPlayer, self).play(path, False, self.replay)
 
     def login(self):
         resp = self.api.login(self.account, self.md5pass)
@@ -71,115 +71,14 @@ class WangYiYunPlayer(SoxPlayer):
         if res["code"] == 200:
             self.plugin.say('已帮你悄悄的签到了，哇哈哈!', cache=True, wait=True)
 
-    def play(self):
-        logger.debug('MusicPlayer play')
-        path = self.playlist[self.idx]['mp3_url']
-        super().stop()
-        super().play(path, False, self.next)
-
-    def replay(self):
-        logger.debug('MusicPlayer replay')
-        path = self.playlist[self.idx]['mp3_url']
-        super().stop()
-        super().play(path, False, onCompleted=self.replay)
-
-    def next(self):
-        logger.debug('MusicPlayer next')
-        super().stop()
-        self.idx = (self.idx+1) % len(self.playlist)
-        self.play()
-
-    def prev(self):
-        logger.debug('MusicPlayer prev')
-        super().stop()
-        self.idx = (self.idx-1) % len(self.playlist)
-        self.play()
-
-    def pause(self):
-        logger.debug('MusicPlayer pause')
-        self.pausing = True
-
-    def stop(self):
-        if self.proc:
-            logger.debug('MusicPlayer stop')
-            # STOP current play process
-            self.last_paused = utils.write_temp_file(str(self.proc.pid), 'pid', 'w')
-            self.onCompleteds = []
-            subprocess.run(['pkill', '-STOP', '-F', self.last_paused])
-
-    def resume(self):
-        logger.debug('MusicPlayer resume')
-        self.pausing = False
-        self.onCompleteds = [self.next]
-        if self.last_paused is not None:
-            logger.debug(self.last_paused)
-            subprocess.run(['pkill', '-CONT', '-F', self.last_paused])
-
-    def is_playing(self):
-        return self.playing
-
-    def is_pausing(self):
-        return self.pausing
-
-    def turnUp(self):
-        system = platform.system()
-        if system == 'Darwin':
-            res = subprocess.run(['osascript', '-e', 'output volume of (get volume settings)'], shell=False, capture_output=True, text=True)
-            volume = int(res.stdout.strip())
-            volume += 20
-            if volume >= 100:
-                volume = 100
-                self.plugin.say('音量已经最大啦', wait=True)
-            subprocess.run(['osascript', '-e', 'set volume output volume {}'.format(volume)])
-        elif system == 'Linux':
-            res = subprocess.run(["amixer sget Master | grep 'Mono:' | awk -F'[][]' '{ print $2 }'"], shell=True, capture_output=True, text=True)
-            logger.info(res.stdout)
-            if res.stdout != '' and res.stdout.strip().endswith('%'):
-                volume = int(res.stdout.strip().replace('%', ''))
-                volume += 20
-                if volume >= 100:
-                    volume = 100
-                    self.plugin.say('音量已经最大啦', wait=True)
-                subprocess.run(['amixer', 'set', 'Master', '{}%'.format(volume)])
-            else:
-                subprocess.run(['amixer', 'set', 'Master', '20%+'])
-        else:
-            self.plugin.say('当前系统不支持调节音量', wait=True)
-        self.resume()
-
-    def turnDown(self):
-        system = platform.system()
-        if system == 'Darwin':
-            res = subprocess.run(['osascript', '-e', 'output volume of (get volume settings)'], shell=False, capture_output=True, text=True)
-            volume = int(res.stdout.strip())
-            volume -= 20
-            if volume <= 20:
-                volume = 20
-                self.plugin.say('音量已经很小啦', wait=True)
-            subprocess.run(['osascript', '-e', 'set volume output volume {}'.format(volume)])
-        elif system == 'Linux':
-            res = subprocess.run(["amixer sget Master | grep 'Mono:' | awk -F'[][]' '{ print $2 }'"], shell=True, capture_output=True, text=True)
-            if res.stdout != '' and res.stdout.endswith('%'):
-                volume = int(res.stdout.replace('%', '').strip())
-                volume -= 20
-                if volume <= 20:
-                    volume = 20
-                    self.plugin.say('音量已经最小啦', wait=True)
-                subprocess.run(['amixer', 'set', 'Master', '{}%'.format(volume)])
-            else:
-                subprocess.run(['amixer', 'set', 'Master', '20%-'])
-        else:
-            self.plugin.say('当前系统不支持调节音量', wait=True)
-        self.resume()
-
     def get_recommend_songs(self):
         datalist = self.api.dig_info(self.api.recommend_playlist(), 'songs')
         return self.pack_info(datalist, "song_id", "song_name", "artist", "album_name", "mp3_url", "quality")
 
     def get_recommend_playlist(self):
         datalist = self.api.dig_info(self.api.recommend_resource(), 'top_playlists')
-        self.multi_playlist = self.pack_info(datalist, "playlist_id", "playlist_name", "creator_name")
-        playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlist][:5]
+        self.multi_playlists = self.pack_info(datalist, "playlist_id", "playlist_name", "creator_name")
+        playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlists][:5]
         say_info = ''
         for idx, name in enumerate(playlist_name, start=1):
             say_info = say_info + '第{}张叫：{}。'.format(idx, name)
@@ -187,11 +86,11 @@ class WangYiYunPlayer(SoxPlayer):
 
     def get_user_playlist(self):
         datalist = self.api.dig_info(self.api.user_playlist(self.userid), 'top_playlists')
-        self.multi_playlist = self.pack_info(datalist, "playlist_id", "playlist_name", "creator_name")
-        if len(self.multi_playlist) > 5:
-            playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlist][:5]
+        self.multi_playlists = self.pack_info(datalist, "playlist_id", "playlist_name", "creator_name")
+        if len(self.multi_playlists) > 5:
+            playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlists][:5]
         else:
-            playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlist]
+            playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlists]
         say_info = ''
         for idx, name in enumerate(playlist_name, start=1):
             say_info = say_info + '第{}张叫：{}。'.format(idx, name)
@@ -202,7 +101,7 @@ class WangYiYunPlayer(SoxPlayer):
         return self.pack_info(datalist, "song_id", "song_name", "artist", "album_name", "mp3_url", "quality")
 
     def get_playlists_portions(self, idx):
-        playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlist][(idx-1)*5:(idx)*5]
+        playlist_name = [each_list['playlist_name'] for each_list in self.multi_playlists][(idx-1)*5:(idx)*5]
         say_info = ''
         for idx, name in enumerate(playlist_name, start=(idx-1)*5+1):
             say_info = say_info + '第{}张叫：{}。'.format(idx, name)
@@ -239,11 +138,11 @@ class Plugin(AbstractPlugin):
         return int(listNumber) if listNumber.isdigit() else cn2an.cn2an(listNumber, "normal")
 
     def handle_playlists(self, input):
-        if any(word in input for word in [u"我要", u"好", u"继续", u"听"]) and len(self.player.multi_playlist) > 5:
+        if any(word in input for word in [u"我要", u"好", u"继续", u"听"]) and len(self.player.multi_playlists) > 5:
             playlists_info = self.player.get_playlists_portions(self.playlist_number_cut)
             self.playlist_number_cut += 1
             logger.info(playlists_info)
-            if self.playlist_number_cut  > math.ceil(len(self.player.multi_playlist)/5):
+            if self.playlist_number_cut  > math.ceil(len(self.player.multi_playlists)/5):
                 self.say(playlists_info + '你想听哪一张呢，就这么多歌单了，要我重新报一次吗？', onCompleted=lambda: self.handle_playlists(self.activeListen()))
             else:
                 self.say(playlists_info + '你想听哪一张呢，还是要不要听下去呢', onCompleted=lambda: self.handle_playlists(self.activeListen()))
@@ -255,22 +154,23 @@ class Plugin(AbstractPlugin):
 
         elif any(word in input for word in ['啥', '什么', '叫']) and self.hasNumbers(input):
             listNumber = self.whichNumber(input) - 1
-            if listNumber < len(self.player.multi_playlist):
-                self.say('第{}歌单叫{}！'.format(listNumber+1, self.player.multi_playlist[listNumber]['playlist_name']), onCompleted=lambda: self.handle_playlists(self.activeListen()))
+            if listNumber < len(self.player.multi_playlists):
+                self.say('第{}歌单叫{}！'.format(listNumber+1, self.player.multi_playlists[listNumber]['playlist_name']), onCompleted=lambda: self.handle_playlists(self.activeListen()))
             else:
-                self.say('都说了只有{}张歌单啦，哼！'.format(len(self.player.multi_playlist)), onCompleted=lambda: self.handle_playlists(self.activeListen()))
+                self.say('都说了只有{}张歌单啦，哼！'.format(len(self.player.multi_playlists)), onCompleted=lambda: self.handle_playlists(self.activeListen()))
 
         elif u"第" in input and self.hasNumbers(input):
             listNumber = self.whichNumber(input) - 1
-            if listNumber < len(self.player.multi_playlist):
+            if listNumber < len(self.player.multi_playlists):
                 self.player.list_idx = listNumber
-                self.player.playlist = self.player.get_playlist_detail(self.player.multi_playlist[listNumber]['playlist_id'])
+                self.player.playlist_info = self.player.get_playlist_detail(self.player.multi_playlists[listNumber]['playlist_id'])
+                self.player.playlist = [each_song['mp3_url'] for each_song in self.player.playlist_info]
                 self.player.shuffle_songs()
-                #logger.debug([each_song['song_name'] for each_song in self.player.playlist])
+                #logger.debug([each_song['song_name'] for each_song in self.player.playlist_info])
                 self.say('选择了第{}张'.format(listNumber+1), wait=True)
                 self.player.play()
             else:
-                self.say('都说了只有{}张歌单啦，哼！'.format(len(self.player.multi_playlist)), onCompleted=lambda: self.handle_playlists(self.activeListen()))
+                self.say('都说了只有{}张歌单啦，哼！'.format(len(self.player.multi_playlists)), onCompleted=lambda: self.handle_playlists(self.activeListen()))
         
         elif any(word in input for word in [u"不要", u"算了", u"不用"]):
             self.say('哼！白浪费帮你找来了这么多歌单。', cache=True)
@@ -287,13 +187,14 @@ class Plugin(AbstractPlugin):
             self.say(u"你得在配置文件调整一下呀，哼！", cache=True)
             return
         if not self.player:
-            self.player = WangYiYunPlayer(profile[self.SLUG]['account'], profile[self.SLUG]['md5pass'], self)
+            self.player = WangYiYunPlayer(profile[self.SLUG]['account'], profile[self.SLUG]['md5pass'], None, self)
 
         try:
             # 插件核心处理逻辑
             ##################################获取每日推荐歌曲####################################
             if any(word in text for word in ['推荐歌曲', '推荐的歌曲', '歌推荐', '每日推荐']):
-                self.player.playlist = self.player.get_recommend_songs()
+                self.player.playlist_info = self.player.get_recommend_songs()
+                self.player.playlist = [each_song['mp3_url'] for each_song in self.player.playlist_info]
                 self.say('一共有{}首推荐歌曲噢！'.format(len(self.player.playlist)), wait=True)
                 self.player.play()
 
@@ -302,42 +203,42 @@ class Plugin(AbstractPlugin):
                 playlists_info = self.player.get_recommend_playlist()
                 self.playlist_number_cut += 1
                 logger.info(playlists_info)
-                self.say('共找到了{}张歌单哦！'.format(len(self.player.multi_playlist)) + playlists_info + '想听哪一张，或者要不要继续听下去呢', onCompleted=lambda: self.handle_playlists(self.activeListen()))
+                self.say('共找到了{}张歌单哦！'.format(len(self.player.multi_playlists)) + playlists_info + '想听哪一张，或者要不要继续听下去呢', onCompleted=lambda: self.handle_playlists(self.activeListen()))
 
             ###################################获取我的歌单####################################
-            elif any(word in text for word in ['我的歌单', '网易云歌单']):
+            elif u"我的歌单" in text or u"网易云歌单" in text:
                 playlists_info = self.player.get_user_playlist()
                 logger.info(playlists_info)
-                if len(self.player.multi_playlist) > 5:
+                if len(self.player.multi_playlists) > 5:
                     self.playlist_number_cut += 1
-                    self.say('你一共有{}张歌单哦！'.format(len(self.player.multi_playlist)) + playlists_info + '想听哪一张，或者要不要继续听下去呢', onCompleted=lambda: self.handle_playlists(self.activeListen()))
+                    self.say('你一共有{}张歌单哦！'.format(len(self.player.multi_playlists)) + playlists_info + '想听哪一张，或者要不要继续听下去呢', onCompleted=lambda: self.handle_playlists(self.activeListen()))
                 else:
-                    self.say('你一共有{}张歌单哦！'.format(len(self.player.multi_playlist)) + playlists_info + '你想听哪一张呢，或者需要我重新报一次吗', onCompleted=lambda: self.handle_playlists(self.activeListen()))
+                    self.say('你一共有{}张歌单哦！'.format(len(self.player.multi_playlists)) + playlists_info + '你想听哪一张呢，或者需要我重新报一次吗', onCompleted=lambda: self.handle_playlists(self.activeListen()))
 
             ###################################询问歌单名字或者当前歌名####################################
             elif self.player.playlist and any(word in text for word in ['啥', '什么', '叫']):
-                if self.player.multi_playlist and u"歌单" in text:
+                if self.player.multi_playlists and u"歌单" in text:
                     if self.player.list_idx:
-                        logger.info(self.player.multi_playlist[self.player.list_idx]['playlist_name'])
-                        self.say('目前播放的歌单叫{}！'.format(self.player.multi_playlist[self.player.list_idx]['playlist_name']), wait=True)
+                        logger.info(self.player.multi_playlists[self.player.list_idx]['playlist_name'])
+                        self.say('目前播放的歌单叫{}！'.format(self.player.multi_playlists[self.player.list_idx]['playlist_name']), wait=True)
                         self.player.resume()
                     else:
                         self.say('你选的推荐歌曲，怎么会有歌单名呢，哼！', cache=True)
                 else:
-                    logger.info(self.player.playlist[self.player.idx])
-                    self.say('这首歌叫{}, 是{}唱的！'.format(self.player.playlist[self.player.idx]['song_name'], self.player.playlist[self.player.idx]['artist']), wait=True)
+                    logger.info(self.player.playlist_info[self.player.idx])
+                    self.say('这首歌叫{}, 是{}唱的！'.format(self.player.playlist_info[self.player.idx]['song_name'], self.player.playlist_info[self.player.idx]['artist']), wait=True)
                     self.player.resume()
 
             ###################################收藏当前播放的歌曲或歌单###########################################
             elif self.player.playlist and self.nlu.hasIntent(parsed, 'SAVE'):
                 if u"歌单" in text and self.player.list_idx:
-                    if self.player.api.subscribe_playlist(self.player.multi_playlist[self.player.list_idx]['playlist_id']):
+                    if self.player.api.subscribe_playlist(self.player.multi_playlists[self.player.list_idx]['playlist_id']):
                         self.say('目前的歌单已收藏成功！', cache=True, wait=True)
                     else:
                         self.say('这歌单收藏失败，估计之前就收藏了吧', cache=True, wait=True)
                     self.player.resume()
                 elif u"歌" in text:
-                    if self.player.api.like_song(self.player.playlist[self.player.idx]['song_id']):
+                    if self.player.api.like_song(self.player.playlist_info[self.player.idx]['song_id']):
                         self.say('这歌已收藏成功啦！', cache=True, wait=True)
                     else:
                         self.say('这歌收藏失败了，估计之前就收藏了吧', cache=True, wait=True)
